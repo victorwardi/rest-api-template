a15b1e855fdd9985bff622a3a9117e909af13120
Victor Caetano Wardi <victor.wardi-ext@jci.com>
2021-07-03 13:22:20 +0100

    Initial commit

---
+++ src/main/java/com/vwardi/restapitemplate/RestApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.vwardi.restapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTemplateApplication.class, args);
+    }
+
+}
